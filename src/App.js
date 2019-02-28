74f777e01ef9756f4f2b559896d01273452fc7b8
배상훈 <bsh0814@bridgetec.co.kr>
2019-02-28 15:30:50 +0900

    로그인 성공

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super();
+
+  }
+
+  state = {
+    id: '',
+    pw: '',
+    loginState: false
+  }
+
+  handleChange = (e) => {
+    console.log("name : ", e.target.name)
+    console.log("value : ", e.target.value);
+
+    const { value } = e.target
+    this.setState({
+      [e.target.name]: value,
+    })
+  }
+
+
+  handleLogin = () => {
+    const { id, pw, loginState } = this.state;
+
+    if ( loginState ) {
+      alert("이미로그인 되있거든??")
+      return;
+    }
+
+
+    const loginId = id;
+    const password = pw;
+    console.log("id : ", id ,", pw : ", pw)
+    axios.post('http://localhost:4000/api/account/login', {loginId, password} )
+      .then( response => {
+        console.log("response : ", response)
+        if (response.data.resultCode === "0000") {
+          alert("success")
+          this.setState({
+            loginState: true
+          })
+        } else {
+          alert("fail. resultCode : "+ response.data.resultCode)
+        }
+      })
+      .catch( err => {
+        console.log("err : ", err)
+        alert("err : "+err)
+      });
+  }
+
+
   render() {
+    const {
+      handleLogin,
+      handleChange,
+    } = this;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="form-login">
+          <div className="login-title">로그인</div>
+
+          <div className='login-input'>
+            <input 
+              name="id"
+              type="text"     
+              className='input' 
+              placeholder="ID"
+              onChange={handleChange}
+              />
+            <input 
+              name="pw"
+              type="password" 
+              className='input' 
+              placeholder="PASSWORD"
+              onChange={handleChange}
+              />
+          </div>
+
+          <div className='login-buttons'>
+            <button className='btn' onClick={handleLogin}>로그인</button>
+          </div>
+        </div>
       </div>
     );
   }
